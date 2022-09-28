3e8b76ecbb5b6ef9d479d00fde8ee6115bacc468
mroccyen <qingshanpeng@126.com>
2022-09-28 22:11:01 +0800

    本地运行参数修改

--- femas-example/femas-example-springcloud-greenwich-consumer-v2/src/main/java/com/tencent/tsf/femas/example/springcloud/ConsumerController.java
+++ femas-example/femas-example-springcloud-greenwich-consumer-v2/src/main/java/com/tencent/tsf/femas/example/springcloud/ConsumerController.java
@@ -84,7 +84,7 @@ public class ConsumerController {
         if (!StringUtils.isEmpty(tagName)) {
             FemasContext.putTag(tagName, tagValue);
         }
-        LOG.info("consumer say hello v1");
+        LOG.info("consumer say hello v2");
         String hello = providerService.hello();
         LOG.info(hello);
         return hello;
